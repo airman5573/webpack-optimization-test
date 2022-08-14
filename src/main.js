a82905d5c659ca9934639271f78683c3dcbd602b
airman5573 <rpf5573@kakao.com>
2022-08-14 21:05:53 +0900

    하나의 파일에 다 정의하면 tree-shaking이 안된다

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { isNull, at, add } from "./lodash";
+import { isNull } from "./lodash";
+import { A, B } from "./test";
 
 const a = 100;
 const b = a > 1000 ? null : "hi";
+A();
+B();
 if (isNull(b)) {
   console.log("B is Null!");
 }
---
+++ src/test.js
@@ -0,0 +1,19 @@
+export const A = () => {
+  const a = 10;
+  console.log("a : ", a);
+};
+
+export const B = () => {
+  const a = 10;
+  console.log("a : ", a);
+};
+
+export const C = () => {
+  const a = 10;
+  console.log("a : ", a);
+};
+
+export const D = () => {
+  const a = 10;
+  console.log("a : ", a);
+};
