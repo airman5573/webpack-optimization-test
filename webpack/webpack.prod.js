273b5fce1e0cc44b1191b0c2691e259695d2bf75
airman5573 <rpf5573@kakao.com>
2022-08-14 18:25:13 +0900

    minimize off

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,22 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [],
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          compress: {
+            dead_code: false,
+            unused: false,
+          },
+        },
+      }),
+    ],
+  },
 });
