ca380c972d58f729b99a12cc042be7fe066c8438
airman5573 <rpf5573@kakao.com>
2022-08-15 21:50:12 +0900

    예제 변경

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { isNull } from "./lodash";
+import { isNull } from "lodash";
 
 const a = window.localStorage.getItem("aa");
+
 if (isNull(a)) {
-  console.log("A is Null!");
-} else {
-  console.log("A is Not Null!");
+  consolelog("b is Null");
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,18 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const TerserPlugin = require("webpack-terser-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "none",
+  devtool: "source-map",
   plugins: [],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mingle: false,
+        },
+      }),
+    ],
+  },
 });
