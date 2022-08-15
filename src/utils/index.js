150178c6bf9378f5f8caff4d8e9a28aef607593e
airman5573 <rpf5573@kakao.com>
2022-08-15 20:42:57 +0900

    예제 변경

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+export const a = () => {
+  console.log("a");
+  return 10;
+};
+
+export const b = () => {
+  console.log("b");
+  return 10;
+};
+
+export const c = () => {
+  console.log("c");
+  return 10;
+};
